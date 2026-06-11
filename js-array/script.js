1a29821a3aae8e67a568da8b65b277edbc90f4ed
PascoKristhine <TTC2550ST0012@edu.tech.ac.jp>
2026-06-11 15:21:22 +0900

    Add files via upload

---
+++ js-array/script.js
@@ -0,0 +1,10 @@
+//　配列の宣言
+let sampleArray = new Array();
+console.log(sampleArray);
+
+//　配列の要素
+let fruits = ["りんご", "みかん", "ぶどう"];
+console.log(fruits);
+
+//配列の要素一部だけ
+console.log(fruits[0]);
---
+++ js-op/script.js
@@ -0,0 +1,59 @@
+/*
+算術演算子
+*/
+
+// 加算(addition)
+let add = 10 + 5;
+console.log(add);
+
+// 文字列の連結
+let str = "Java" + "Script";
+console.log(str);
+
+let result1 = 3 + 3;
+let result2 = "3" + "3";
+
+console.log(result1);
+console.log(result2);
+
+// 減算（substract)
+let sub = 10 - 5;
+console.log(sub);
+
+// 乗算（multiplication)
+let mul = 10 * 5; // 10 x 5
+console.log(mul);
+
+// 除算（division)
+let div = 10 / 5;
+console.log(div);
+
+// インクリメント（increment)
+let incr = 10;
+incr++;
+console.log(incr);
+
+// デクリメント（decrement)
+let decr = 10;
+decr--;
+console.log(decr);
+
+/*
+代入演算子
+*/
+
+// 加算して代入
+let add2 = 1;
+add2 += 2;
+console.log(add2);
+
+// 文字列を連結して代入
+let str2 = "Java";
+str2 += "Script";
+console.log(str2);
+
+//減算して代入
+let sub2 = 10;
+sub2 -= 5;
+console.log(sub2);
+
